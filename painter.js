4a781e32a1852d82fd19a1a54dd66ee1719871c9
heejaechang <heejae0704@gmail.com>
2020-03-01 19:16:25 +0900

    painter enhanced

--- painter.js
+++ painter.js
@@ -24,6 +24,21 @@ function createCanvas(canvasWidth, canvasHeight) {
     },
     false
   );
+
+  canvas.addEventListener(
+    "dragover",
+    function(e) {
+      e.preventDefault();
+    },
+    false
+  );
+
+  canvas.addEventListener("drop", function(e) {
+    var files = e.dataTrasfer.files;
+    if (files[0].type.substring(0, 6) !== "image/") return;
+    loadImageURL(ctx, URL.createObjectURL(files[0]));
+    e.preventDefault();
+  });
   return [canvas, ctx];
 }
 function relativePosition(event, element) {
@@ -179,3 +194,164 @@ controls.alpha = function(ctx) {
   );
   return label;
 };
+
+controls.save = function(ctx) {
+  var input = elt("input", { type: "button", value: "저장" });
+  var label = elt("label", null, " ", input);
+  input.addEventListener(
+    "click",
+    function(e) {
+      var dataURL = ctx.canvas.toDataURL();
+      open(dataURL, "save");
+    },
+    false
+  );
+  return label;
+};
+
+var filterTools = Object.create(null);
+controls.filter = function(ctx) {
+  var DEFAULT_FILTER = 0;
+  var select = elt("select", null);
+  var label = elt("label", null, " ", select);
+  select.appendChild(elt("option", { value: "filter" }, "필터"));
+  for (var name in filterTools) {
+    select.appendChild(elt("option", { value: name }, name));
+  }
+  select.selectedIndex = DEFAULT_FILTER;
+  select.addEventListener(
+    "change",
+    function(e) {
+      var filterTool = this.children[this.selectedIndex].value;
+      var inputImage = ctx.getImageData(
+        0,
+        0,
+        ctx.canvas.width,
+        ctx.canvas.height
+      );
+      var outputImage = filterTools[filterTool](inputImage);
+      ctx.putImageData(outputImage, 0, 0);
+      select.selectedIndex = DEFAULT_FILTER;
+    },
+    false
+  );
+  return label;
+};
+
+function weightedAverageFilter(image, n, Weight, keepBrightness, offset) {
+  var width = image.width,
+    height = image.height;
+  var outputImage = new ImageData(width, height);
+  for (var x = 0; x < width; x++) {
+    for (var y = 0; y < height; y++) {
+      var iR = 4 * (width * y + x);
+      for (var i = 0; i < 3; i++) {
+        var average = 0,
+          weightSum = 0;
+        for (ix = -n; ix <= n; ix++) {
+          var xp = x + ix;
+          if (xp < 0 || xp >= width) continue;
+          for (iy = -n; iy <= n; iy++) {
+            var yp = y + iy;
+            if (yp < 0 || yp >= height) continue;
+            var w = Weight[iy + n][ix + n];
+            weightSum += w;
+            average += w * image.data[4 * (width * yp + xp) + i];
+          }
+        }
+        if (keepBrightness) {
+          average /= weightSum;
+        }
+        outputImage.data[iR + i] = average + offset;
+      }
+      outputImage.data[iR + 3] = image.data[iR + 3];
+    }
+  }
+  return outputImage;
+}
+
+// * 블러 필터
+filterTools.blur = function(inputImage) {
+  var size = 2;
+  var W = [];
+  for (var i = 0; i <= 2 * size; i++) {
+    W[i] = [];
+    for (var j = 0; j <= 2 * size; j++) {
+      W[i][j] = 1;
+    }
+  }
+  return weightedAverageFilter(inputImage, size, W, true, 0);
+};
+// * 샤프 필터
+filterTools.sharp = function(inputImage) {
+  var W = [
+    [0, -1, 0],
+    [-1, 5, -1],
+    [0, -1, 0]
+  ];
+  return weightedAverageFilter(inputImage, 1, W, false, 0);
+};
+// * 엠보싱 필터
+filterTools.emboss = function(inputImage) {
+  var W = [
+    [-1, 0, 0],
+    [0, 0, 0],
+    [0, 0, 1]
+  ];
+  return weightedAverageFilter(inputImage, 1, W, false, 128);
+};
+// * 테두리 강조
+filterTools.edgeDetection = function(inputImage) {
+  var W = [
+    [-1, -1, -1],
+    [-1, 8, -1],
+    [-1, -1, -1]
+  ];
+  return weightedAverageFilter(inputImage, 1, W, false, 0);
+};
+
+controls.file = function(ctx) {
+  var input = elt("input", { type: "file" });
+  var label = elt("label", null, " ", input);
+  input.addEventListener(
+    "change",
+    function(e) {
+      if (input.files.length == 0) return;
+      var reader = new FileReader();
+      reader.onload = function() {
+        loadImageURL(ctx, reader.result);
+      };
+      reader.readAsDataURL(input.files[0]);
+    },
+    false
+  );
+  return label;
+};
+
+function loadImageURL(ctx, url) {
+  var image = document.createElement("img");
+  image.onload = function() {
+    var factor = Math.min(
+      ctx.canvas.width / this.width,
+      ctx.canvas.height / this.height
+    );
+    var wshift = (ctx.canvas.width - factor * this.width) / 2;
+    var hshift = (ctx.canvas.height - factor * this.height) / 2;
+    var savedColor = ctx.fillStyle;
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.drawImage(
+      image,
+      0,
+      0,
+      this.width,
+      this.height,
+      wshift,
+      hshift,
+      this.width * factor,
+      this.height * factor
+    );
+    ctx.fillStyle = savedColor;
+  };
+  image.src = url;
+}
